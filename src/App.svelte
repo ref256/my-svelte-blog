74fdb4478441ed1f2ce4840ef7718d50d8b720d4
refactor256 <refactor256@gmail.com>
2022-05-22 09:36:23 +0300

    Initial commit: svelte + ts + tailwind

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Tailwindcss from './Tailwindcss.svelte';
+
+	export let name: string;
+</script>
+
+<Tailwindcss />
+<main class="md:max-w-[240px] text-center p-4 mx-auto">
+	<h1 class="text-red-500 uppercase font-thin text-7xl">Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
